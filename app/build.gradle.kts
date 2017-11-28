35376e03850a95d99170b246f49f89350cd45408
Hendra Anggrian <hendraanggrian@gmail.com>
2017-11-28 22:48:44 +0700

    now targeting 1.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,15 +27,16 @@ dependencies {
     implementation(kotlin("stdlib", kotlinVersion))
     implementation(kotfx(kotfxVersion))
 
-    implementation("io.reactivex.rxjava2:rxjava:$rxjavaVersion")
-    implementation("io.reactivex.rxjava2:rxjavafx:$rxkotlinVersion")
-    implementation("io.reactivex.rxjava2:rxkotlin:$rxkotlinVersion")
+    implementation(rx("java", rxjavaVersion))
+    implementation(rx("javafx", rxkotlinVersion))
+    implementation(rx("kotlin", rxkotlinVersion))
+
+    implementation(apache("commons", "math3", commonsMathVersion))
+    implementation(apache("poi", "ooxml", poiVersion))
 
     implementation("com.wijayaprinting:mysql:$mysqlVersion")
     implementation("com.hendraanggrian:rxexposed:$rxexposedVersion")
     implementation("com.google.guava:guava:$guavaVersion-jre")
-    implementation("org.apache.commons:commons-math3:$commonsMathVersion")
-    implementation("org.apache.poi:poi-ooxml:$poiVersion")
     implementation("org.slf4j:slf4j-log4j12:$slf4jVersion")
 
     testImplementation(junit(junitVersion))
--- buildSrc/src/main/kotlin/extensions.kt
+++ buildSrc/src/main/kotlin/extensions.kt
@@ -5,7 +5,7 @@ const val releaseArtifact = "javafx"
 const val releaseVersion = "0.3"
 
 /** Set false when building app, true when building stage. */
-const val isDebug = true
+const val isDebug = false
 
 const val mysqlVersion = "0.3"
 const val kotlinVersion = "1.1.60"
@@ -23,4 +23,6 @@ const val slf4jVersion = "1.7.25"
 const val junitVersion = "4.12"
 
 fun DependencyHandler.kotfx(version: Any) = "com.hendraanggrian:kotfx:$version"
+fun DependencyHandler.rx(module: Any, version: Any) = "io.reactivex.rxjava2:rx$module:$version"
+fun DependencyHandler.apache(module1: Any, module2: Any, version: Any) = "org.apache.$module1:$module1-$module2:$version"
 fun DependencyHandler.junit(version: Any) = "junit:junit:$version"

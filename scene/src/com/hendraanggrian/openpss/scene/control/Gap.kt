3afb31d0bd82af770e0c9c4b83cca125ce130f12
Hendra Anggrian <hendraanggrian@gmail.com>
2018-03-20 03:24:22 +0700

    Gap replaces Separator for Toolbar buttons (stubs)

---
+++ scene/src/com/hendraanggrian/openpss/scene/control/Gap.kt
@@ -0,0 +1,28 @@
+@file:Suppress("NOTHING_TO_INLINE", "UNUSED")
+
+package com.hendraanggrian.openpss.scene.control
+
+import javafx.scene.layout.Region
+import ktfx.layouts.ChildManager
+import ktfx.layouts.ItemManager
+import ktfx.layouts.LayoutDsl
+import ktfx.scene.layout.widthMin
+
+class Gap(width: Int = 32) : Region() {
+
+    init {
+        widthMin = width
+    }
+}
+
+inline fun gap(
+    noinline init: ((@LayoutDsl Gap).() -> Unit)? = null
+): Gap = Gap().apply { init?.invoke(this) }
+
+inline fun ChildManager.gap(
+    noinline init: ((@LayoutDsl Gap).() -> Unit)? = null
+): Gap = com.hendraanggrian.openpss.scene.control.gap(init).add()
+
+inline fun ItemManager.gap(
+    noinline init: ((@LayoutDsl Gap).() -> Unit)? = null
+): Gap = com.hendraanggrian.openpss.scene.control.gap(init).add()

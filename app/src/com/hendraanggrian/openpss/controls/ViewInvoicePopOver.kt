df90771a060cc66d64c2c2e911ab28d98df82d0d
Hendra Anggrian <hendraanggrian@gmail.com>
2018-05-18 01:49:48 +0700

    Testing wide invoice

--- app/src/com/hendraanggrian/openpss/controls/ViewInvoicePopOver.kt
+++ app/src/com/hendraanggrian/openpss/controls/ViewInvoicePopOver.kt
@@ -11,22 +11,34 @@ import com.hendraanggrian.openpss.db.schemas.Invoice
 import com.hendraanggrian.openpss.db.transaction
 import com.hendraanggrian.openpss.localization.Language
 import com.hendraanggrian.openpss.localization.Resourced
-import com.hendraanggrian.openpss.util.PATTERN_DATE
-import com.hendraanggrian.openpss.util.PATTERN_TIME
+import com.hendraanggrian.openpss.util.PATTERN_DATETIME
 import com.hendraanggrian.openpss.util.currencyConverter
 import com.hendraanggrian.openpss.util.getFont
 import com.hendraanggrian.openpss.util.numberConverter
-import javafx.geometry.Pos.BOTTOM_CENTER
+import javafx.geometry.HPos.RIGHT
+import javafx.geometry.Insets
 import javafx.geometry.Pos.CENTER
+import javafx.geometry.Pos.CENTER_LEFT
 import javafx.geometry.Pos.CENTER_RIGHT
 import javafx.scene.Node
 import javafx.scene.control.Label
+import javafx.scene.layout.Border
+import javafx.scene.layout.BorderStroke
+import javafx.scene.layout.BorderStrokeStyle.DASHED
+import javafx.scene.layout.BorderStrokeStyle.SOLID
+import javafx.scene.layout.BorderWidths.DEFAULT
+import javafx.scene.layout.CornerRadii.EMPTY
 import javafx.scene.layout.Priority.ALWAYS
+import javafx.scene.paint.Color.BLACK
 import ktfx.layouts.LayoutManager
+import ktfx.layouts.borderPane
 import ktfx.layouts.button
+import ktfx.layouts.columnConstraints
+import ktfx.layouts.gridPane
 import ktfx.layouts.hbox
 import ktfx.layouts.label
 import ktfx.layouts.line
+import ktfx.layouts.pane
 import ktfx.layouts.region
 import ktfx.layouts.vbox
 import java.util.ResourceBundle
@@ -48,85 +60,95 @@ class ViewInvoicePopOver(invoice: Invoice) : SimplePopOver(object : Resourced {
             employee = Employees[invoice.employeeId].single()
             customer = Customers[invoice.customerId].single()
         }
-        vbox(16.0) {
-            minWidth = MAX_WIDTH
-            maxWidth = MAX_WIDTH
-            alignment = CENTER_RIGHT
-            vbox {
-                alignment = CENTER
-                invoiceHeaders.forEachIndexed { index, s ->
-                    when (index) {
-                        0 -> boldLabel(s)
-                        else -> label(s)
+        pane {
+            border = Border(BorderStroke(BLACK, DASHED, EMPTY, DEFAULT))
+            vbox(16.0) {
+                padding = Insets(16.0)
+                minWidth = MAX_WIDTH
+                maxWidth = MAX_WIDTH
+                minHeight = MAX_HEIGHT
+                maxHeight = MAX_HEIGHT
+                borderPane {
+                    left = ktfx.layouts.vbox {
+                        invoiceHeaders.forEachIndexed { index, s ->
+                            when (index) {
+                                0 -> boldLabel(s)
+                                else -> label(s)
+                            }
+                        }
+                    } align CENTER_LEFT
+                    right = ktfx.layouts.label(getString(R.string.invoice)) {
+                        font = getFont(R.font.sf_pro_text_bold, 32)
+                    } align CENTER_RIGHT
+                }
+                line(endX = MAX_WIDTH - 32.0)
+                gridPane {
+                    columnConstraints {
+                        constraints { hgrow = ALWAYS }
+                        constraints { halignment = RIGHT }
                     }
+                    boldLabel(customer.name, 24) col 0 row 0
+                    label("${getString(R.string.employee)}: ${transaction {
+                        Employees[invoice.employeeId].single().name
+                    }}") col 0 row 1
+                    label(invoice.dateTime.toString(PATTERN_DATETIME)) col 1 row 0
+                    boldLabel("#${invoice.no}", 18) col 1 row 1
                 }
-            }
-            vbox {
-                hbox {
-                    alignment = BOTTOM_CENTER
-                    boldLabel("#${invoice.no}", 18)
-                    vbox {
-                        alignment = CENTER_RIGHT
-                        label(invoice.dateTime.toString(PATTERN_DATE))
-                        label(invoice.dateTime.toString(PATTERN_TIME))
-                    } hpriority ALWAYS
-                }
-                boldLabel(customer.name, 18)
-                label("${getString(R.string.employee)}: ${employee.name}")
-            }
-            invoice.plates.run {
-                if (isNotEmpty()) vbox {
-                    boldLabel(getString(R.string.plate))
-                    forEach {
-                        label(it.title)
-                        hbox {
-                            label("  ${it.machine} ${numberConverter.toString(it.qty)} x " +
-                                currencyConverter.toString(it.price))
-                            region() hpriority ALWAYS
-                            label(currencyConverter.toString(it.total))
+                invoice.plates.run {
+                    if (isNotEmpty()) vbox {
+                        boldLabel(getString(R.string.plate))
+                        forEach {
+                            label(it.title)
+                            hbox {
+                                label("  ${it.machine} ${numberConverter.toString(it.qty)} x " +
+                                    currencyConverter.toString(it.price))
+                                region() hpriority ALWAYS
+                                label(currencyConverter.toString(it.total))
+                            }
                         }
                     }
                 }
-            }
-            invoice.offsets.run {
-                if (isNotEmpty()) vbox {
-                    boldLabel(getString(R.string.offset))
-                    forEach {
-                        label(it.title)
-                        hbox {
-                            label("  ${it.machine} ${it.typedTechnique.toString(this@ViewInvoicePopOver)} " +
-                                numberConverter.toString(it.qty))
-                            region() hpriority ALWAYS
-                            label(currencyConverter.toString(it.total))
+                invoice.offsets.run {
+                    if (isNotEmpty()) vbox {
+                        boldLabel(getString(R.string.offset))
+                        forEach {
+                            label(it.title)
+                            hbox {
+                                label("  ${it.machine} ${it.typedTechnique.toString(this@ViewInvoicePopOver)} " +
+                                    numberConverter.toString(it.qty))
+                                region() hpriority ALWAYS
+                                label(currencyConverter.toString(it.total))
+                            }
                         }
                     }
                 }
-            }
-            invoice.others.run {
-                if (isNotEmpty()) vbox {
-                    boldLabel(getString(R.string.others))
-                    forEach {
-                        label(it.title)
-                        hbox {
-                            label("  ${numberConverter.toString(it.qty)} x " + currencyConverter.toString(it.price))
-                            region() hpriority ALWAYS
-                            label(currencyConverter.toString(it.total))
+                invoice.others.run {
+                    if (isNotEmpty()) vbox {
+                        boldLabel(getString(R.string.others))
+                        forEach {
+                            label(it.title)
+                            hbox {
+                                label("  ${numberConverter.toString(it.qty)} x " + currencyConverter.toString(it.price))
+                                region() hpriority ALWAYS
+                                label(currencyConverter.toString(it.total))
+                            }
                         }
                     }
                 }
-            }
-            boldLabel(currencyConverter.toString(invoice.total), 18)
-            if (invoice.note.isNotBlank()) {
-                vbox {
-                    boldLabel(getString(R.string.note))
-                    label(invoice.note)
+                boldLabel(currencyConverter.toString(invoice.total), 18)
+                borderPane {
+                    left = ktfx.layouts.vbox {
+                        border = Border(BorderStroke(BLACK, SOLID, EMPTY, DEFAULT))
+                        boldLabel(getString(R.string.note))
+                        label(invoice.note)
+                    }
+                    right = ktfx.layouts.vbox {
+                        alignment = CENTER
+                        region { prefHeight = 50.0 }
+                        line(endX = 200.0)
+                        label(getString(R.string.signature))
+                    }
                 }
-            }
-            vbox {
-                alignment = CENTER
-                region { prefHeight = 50.0 }
-                line(endX = 200.0)
-                label(getString(R.string.signature))
             }
         }
         buttonBar.run {
@@ -138,7 +160,8 @@ class ViewInvoicePopOver(invoice: Invoice) : SimplePopOver(object : Resourced {
     }
 
     private companion object {
-        const val MAX_WIDTH = 283.46456693 // equivalent to 7.5cm
+        const val MAX_WIDTH = 912.0 // equivalent to 9.5"
+        const val MAX_HEIGHT = 528.0 // equivalent to 5.5"
 
         fun LayoutManager<Node>.boldLabel(
             text: String,

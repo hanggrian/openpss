ae2bd59d9656db3895340b99960bbbcb32bb2ebb
Hendra Anggrian <hendraanggrian@gmail.com>
2019-02-01 01:58:17 +0700

    3.5.0-alpha02 is required by Android Studio Preview

--- buildSrc/src/versions.kt
+++ buildSrc/src/versions.kt
@@ -10,7 +10,7 @@ const val SDK_MIN = 14
 const val SDK_TARGET = 28
 const val BUILD_TOOLS = "28.0.3"
 
-internal const val VERSION_ANDROID_PLUGIN = "3.3.0"
+internal const val VERSION_ANDROID_PLUGIN = "3.5.0-alpha02"
 const val VERSION_MULTIDEX = "2.0.1"
 const val VERSION_ANDROIDX = "1.1.0"
 const val VERSION_ESPRESSO = "3.1.1"
